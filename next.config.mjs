05a0708ebe785c1e4a41ed82bcfa4f19159da9fa
Malcolm C <55069873+mjcdata@users.noreply.github.com>
2026-09-08 17:33:12 +0000

    Fix GitHub Pages asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  basePath: isProd ? '/mjc-portfolio' : '',
+  assetPrefix: isProd ? '/mjc-portfolio/' : '',
 };
 
 export default nextConfig;
